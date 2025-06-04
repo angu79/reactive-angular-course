388c4e20b64edf016de5e0a34e03189929d4abff
DESKTOP-8OCJD81\DJPB <arifsoedibyo@gmail.com>
2025-06-04 07:42:52 +0700

    17. Error Handling and the Messages Component

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -11,7 +11,7 @@ import {tap} from 'rxjs/operators';
 })
 export class MessagesComponent implements OnInit {
 
-
+showMessages: boolean = false;
   constructor() {
 
   }
@@ -23,7 +23,7 @@ export class MessagesComponent implements OnInit {
 
 
   onClose() {
-
+this.showMessages = false;
 
   }
 
