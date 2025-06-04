1021919c72b822e01356b21ebe8c627f84d467a3
DESKTOP-8OCJD81\DJPB <arifsoedibyo@gmail.com>
2025-06-04 08:02:08 +0700

    18. Error Handling with the catchError RxJs operator

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { Course, sortCoursesBySeqNo } from "../model/course";
-import { Observable } from "rxjs";
-import { finalize, map } from "rxjs/operators";
+import { Observable, throwError } from "rxjs";
+import { catchError, finalize, map } from "rxjs/operators";
 import { CoursesService } from "../services/courses.service";
 import { LoadingService } from "../loading/loading.service";
+import { MessagesService } from "../messages/messages.service";
 
 @Component({
   selector: "home",
@@ -16,21 +17,29 @@ export class HomeComponent implements OnInit {
 
   advancedCourses$: Observable<Course[]>;
 
-  constructor(private coursesService: CoursesService, private loadingService: LoadingService) {}
+  constructor(
+    private coursesService: CoursesService,
+    private loadingService: LoadingService,
+    private messagesService: MessagesService
+  ) {}
 
   ngOnInit() {
     this.reloadCourses();
   }
 
   reloadCourses() {
-
     const courses$ = this.coursesService
       .loadAllCourses()
       .pipe(map((courses) => courses.sort(sortCoursesBySeqNo)),
+      catchError((err) => {
+        const message = "Could not load courses.";
+        this.messagesService.showErrors(message);
+        console.log(message, err);
+        return throwError(err);
+      })
     );
 
     const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$);
-
 
     this.beginnerCourses$ = loadCourses$.pipe(
       map((courses) =>
